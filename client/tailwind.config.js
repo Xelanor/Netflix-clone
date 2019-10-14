2b30aa906b3a0e90adf128b046955778e0aa3ff5
Xelanor <berozel@hotmail.com>
2019-10-14 22:44:12 +0300

    Tailwind css added

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        clone: '#282828',
+        clone2: '#C4C4C4',
+        clone3: '#B5B3B3',
+        tekno4: '#0B032D',
+        tekno5: '#545454'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
