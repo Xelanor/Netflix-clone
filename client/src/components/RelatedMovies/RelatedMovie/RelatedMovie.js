37bcbdfe6a03d287090d76338815799adc9f01a0
Xelanor <44201710+Xelanor@users.noreply.github.com>
2019-10-25 10:54:24 +0300

    Update RelatedMovie.js

--- client/src/components/RelatedMovies/RelatedMovie/RelatedMovie.js
+++ client/src/components/RelatedMovies/RelatedMovie/RelatedMovie.js
@@ -8,7 +8,7 @@ const RelatedMovie = (props) => {
     <Link to="/izle?f=123asdf235123">
       <div className="flex mr-1 mb-6">
         <div className="flex relative inline-block mr-2 related-movie">
-          <img src="https://i.ytimg.com/vi/TcMBFSGVi1c/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCfjWZT0nq_NRhPPPiJMeyDpO1zNw" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjgQZ7pKoBDr1Mg-JRNrI7iOqcEc7wAIXncvXmkNydrhWJr0Zw" />
         </div>
         <div className="flex-1">
           <div title="asdfasdf asdfasdf asdfasdf asdfasdf asdfasdf asdfasdf asdfasdf asdfasdf" className="flex text-white mb-4 text-xl xl:text-2xl font-medium movie-desc">
